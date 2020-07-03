486ab2c56a69b55a0b6e7d152371fd1f11aaf4c6
Reilly Chase <rchase@rchase.com>
2020-07-03 19:35:39 -0400

    Changed relative path to absolute for amount.mp3

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import stripe
 import os
 import configparser
+from gtts import gTTS
 
 config = configparser.ConfigParser()
 config.read('/home/pi/cash-register-pi/config.ini')
@@ -13,7 +14,11 @@ for event in events:
     if event["id"] == last_event:
         break
     if event["type"] == "charge.succeeded":
+        text_to_say = str(int(event["data"]["object"]["amount"]) / 100) + " dollars"
+        amount_mp3 = gTTS(text=text_to_say, lang="en", slow=False)
+        amount_mp3.save("/home/pi/cash-register-pi/sounds/amount.mp3")
         os.system("/usr/bin/cvlc /home/pi/cash-register-pi/sounds/ka-ching.mp3 -q --play-and-exit")
+        os.system("/usr/bin/cvlc /home/pi/cash-register-pi/sounds/amount.mp3 -q --play-and-exit")
     if event["type"] == "customer.subscription.created":
         os.system("/usr/bin/cvlc /home/pi/cash-register-pi/sounds/squad-goin-up.mp3 -q --play-and-exit")
 
